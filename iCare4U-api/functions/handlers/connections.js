c85d42c36f4f72d34487aa34ac1d0506db829bd8
Marcin <porterneon@gmail.com>
2020-01-02 18:19:04 +0100

    refactir connections handler

--- iCare4U-api/functions/handlers/connections.js
+++ iCare4U-api/functions/handlers/connections.js
@@ -1,33 +1,43 @@
 const { db } = require("../util/admin");
 
-const { getPatientDetails } = require("./patients");
+async function getPatientIdsByUserId(userId) {
+  const connections = await db
+    .collection("connections")
+    .where("userId", "==", userId)
+    .get();
+
+  let patientIds = [];
+  if (connections) {
+    connections.docs.forEach(element => {
+      patientIds.push(element.data().patientId);
+    });
+  }
+  return patientIds;
+}
+
+async function getPatientsByIds(patientIds) {
+  let patients = [];
+
+  const data = await db
+    .collection("patient")
+    .where("patientId", "in", patientIds)
+    .get();
+
+  data.forEach(doc => {
+    patients.push(doc.data());
+  });
+
+  return patients;
+}
 
 exports.getPatientsByUser = async (req, res) => {
   try {
     console.log(req.params);
-    const connections = await db
-      .collection("connections")
-      .where("userId", "==", req.params.userId)
-      .get();
 
-    let patientIds = [];
-    if (connections) {
-      connections.docs.forEach(element => {
-        patientIds.push(element.data().patientId);
-      });
-    }
+    let patientIds = await getPatientIdsByUserId(req.params.userId);
 
-    let patients = [];
+    let patients = await getPatientsByIds(patientIds);
 
-    const data = await db
-      .collection("patient")
-      .where("patientId", "in", patientIds)
-      .get();
-
-    data.forEach(doc => {
-      patients.push(doc.data());
-    });
-    console.log(patients);
     return res.json(patients);
   } catch (e) {
     return res.status(500).json(e);
