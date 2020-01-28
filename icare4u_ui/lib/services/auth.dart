4482974b270f728cb4504b345b389ebf438d3293
Neon <porterneon@gmail.com>
2020-01-28 21:41:35 +0100

    add user model

--- icare4u_ui/lib/services/auth.dart
+++ icare4u_ui/lib/services/auth.dart
@@ -1,14 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:icare4u_ui/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
 
   // sign in anon
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
