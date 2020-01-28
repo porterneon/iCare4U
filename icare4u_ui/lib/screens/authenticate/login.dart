4f23b44f71d16ccfd091fa3c511e1fadc75e459b
Marcin <porterneon@gmail.com>
2020-01-28 20:39:08 +0100

    define global background color

--- icare4u_ui/lib/screens/authenticate/login.dart
+++ icare4u_ui/lib/screens/authenticate/login.dart
@@ -254,29 +254,13 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
         child: GestureDetector(
           onTap: () => FocusScope.of(context).unfocus(),
           child: Stack(
             children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0xFF73AEF5),
-                      Color(0xFF61A4F1),
-                      Color(0xFF478DE0),
-                      Color(0xFF398AE5),
-                    ],
-                    stops: [0.1, 0.4, 0.7, 0.9],
-                  ),
-                ),
-              ),
               Container(
                 height: double.infinity,
                 child: SingleChildScrollView(
--- icare4u_ui/lib/screens/wrapper.dart
+++ icare4u_ui/lib/screens/wrapper.dart
@@ -7,6 +7,28 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either the Home or Authenticate widget
-    return LoginScreen();
+    return Scaffold(
+      body: Container(
+        // Add box decoration
+        decoration: BoxDecoration(
+          // Box decoration takes a gradient
+          gradient: LinearGradient(
+            // Add one stop for each color. Stops should increase from 0 to 1
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF73AEF5),
+              Color(0xFF61A4F1),
+              Color(0xFF478DE0),
+              Color(0xFF398AE5),
+            ],
+            stops: [0.1, 0.4, 0.7, 0.9],
+          ),
+        ),
+        child: Center(
+          child: LoginScreen(),
+        ),
+      ),
+    );
   }
 }
