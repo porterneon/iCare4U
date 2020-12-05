1a3a07609be23d1c6a1d38c7fe47774e4d87fd06
Marcin Wielgus <porterneon@gmail.com>
2020-12-05 21:14:38 +0100

    jwt user autentication with mongodb

---
+++ iCare4U-mongodb-api/server.js
@@ -0,0 +1,32 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+function helloWorld(request, response) {
+    response.send("Hello from iCare4U!");
+};
+
+app.get("/", helloWorld);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
